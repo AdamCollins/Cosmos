6e842b56ba1d641b6cfa5a32054f86f2c77d211c
AdamCollins <adamcollinssecure@gmail.com>
2017-07-07 23:02:37 -0700

    Setup front-end login validation

--- app/public/js/register.js
+++ app/public/js/register.js
@@ -1,4 +1,36 @@
-$('#RegisterBtn').click(function(e){
-    e.preventDefault();
-    console.log('Register completed');
+$('#RegisterBtn').click(function(e) {
+  e.preventDefault();
+  let username = $('#usernameTF').val();
+  if(username.length<0)
+    makeInvalid($('#usernameTF'));
+  let password = $('#passwordTF').val();
+  let passwordConfig = $('#passwordConfTF').val();
+  if (password.length >= 6) {
+    if (password === passwordConfig) {
+      //if(usernameAvalible(userame))
+      makeValid($('#passwordTF'));
+      makeValid($('#passwordConfTF'));
+      makeNeutral($('#usernameTF'));
+      //  register  //TODO recheck during post
+    } else {
+      Materialize.toast('Passwords do not match', 2000);
+      $("#passwordTF").removeClass('valid').addClass('invalid');
+      $("#passwordConfTF").removeClass('valid').addClass('invalid');
+    }
+  } else {
+    Materialize.toast('Password must be atleast 6 characters', 2000);
+    $("#passwordTF").addClass('invalid');
+  }
 });
+
+function makeValid(item) {
+  $(item).removeClass('invalid').addClass('valid');
+}
+
+function makeInvalid(item) {
+  $(item).removeClass('valid').addClass('invalid');
+}
+
+function makeNeutral(item) {
+  $(item).removeClass('invalid').removeClass('valid');
+}

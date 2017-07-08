1b09ebc18a9323bba8b8103516500c5248612366
Andy Li <andylibc@gmail.com>
2017-07-07 23:01:02 -0700

    setting up mongo

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,4 +1,14 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/learning_mongo'
+
+MongoClient.connect(url, function(err, db){
+  console.log('connected successfully to sever');
+
+  db.close();
+});
+
+
 var router = express.Router();
 var postData = require('../data/posts.json');
 router.get('/api',function(req, res){

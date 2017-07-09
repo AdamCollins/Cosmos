44219ec3a7eacc22c712d68e9e262586bc3966cf
AdamCollins <adamcollinssecure@gmail.com>
2017-07-09 01:58:39 -0700

    small changes

--- app/routes/api.js
+++ app/routes/api.js
@@ -2,41 +2,39 @@ var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
 var config = require('../data/config');
-var dbName = "cosmosdb"; 
+var dbName = "cosmosdb";
 var dbpassword = config.password;
 
-//var uri = "mongodb://kay:myRealPassword@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
-var url = 'mongodb://cosmos:'+dbpassword+'@cluster0-shard-00-00-oe5ks.mongodb.net:27017,cluster0-shard-00-01-oe5ks.mongodb.net:27017,cluster0-shard-00-02-oe5ks.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+var url = 'mongodb://cosmos:' + dbpassword + '@cluster0-shard-00-00-oe5ks.mongodb.net:27017,cluster0-shard-00-01-oe5ks.mongodb.net:27017,cluster0-shard-00-02-oe5ks.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 var postData = require('../data/posts.json');
 var bodyParser = require('body-parser');
-var database = null
+var database = null;
 
-MongoClient.connect(url, function(err, db){
-  if(err){
+MongoClient.connect(url, function(err, db) {
+  if (err) {
     console.log(err);
   }
-  console.log('connected successfully to database');
-  console.log('db:prior'+db)
   database = db
-  console.log('db:after'+db)
-  findDocument(db, function(){
+  findDocument(db, function() {
     db.close();
   })
 });
 
-function insertPost(db, postData, callback){
+function insertPost(db, postData, callback) {
   console.log('here')
   var collection = db.collection('users');
-  console.log("COL:"+collection)
+  console.log("COL:" + collection)
   var post = postData.text
-  collection.insert({"name":post})
+  collection.insert({
+    "name": post
+  })
   console.log('here2')
   callback
 }
 
-function findDocument(db, callback){
+function findDocument(db, callback) {
   var collection = db.collection('users');
-  collection.find().toArray(function(err, doc){
+  collection.find().toArray(function(err, doc) {
     //console.log(doc);
     callback
   })
@@ -44,14 +42,16 @@ function findDocument(db, callback){
 
 
 
-router.get('/api',function(req, res){
+router.get('/api', function(req, res) {
   res.json(postData);
 });
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended:false}));
-router.post('/api',function(req,res){
-  insertPost(database, req.body, function(){
+router.use(bodyParser.urlencoded({
+  extended: false
+}));
+router.post('/api', function(req, res) {
+  insertPost(database, req.body, function() {
     database.close()
   })
   console.log(req.body);

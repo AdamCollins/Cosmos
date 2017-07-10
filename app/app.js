2f26a7eca91ef53dacd8aae1a37b7c448e2b82c6
Andy Li <andylibc@gmail.com>
2017-07-09 23:34:28 -0700

    fixed api page

--- app/app.js
+++ app/app.js
@@ -1,20 +1,15 @@
 var express = require('express');
-
 var reload = require('reload');
 var app = express();
 var postData = require('./data/posts.json');
 
-
 app.set('port',3000);
-
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/api'));
-
-
 
 var port = app.get('port')
 var server = app.listen(port, function(){
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,59 +1,41 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:false}));
 var config = require('../data/config');
 var dbName = "cosmosdb"; 
 var dbpassword = config.password;
-
-//var uri = "mongodb://kay:myRealPassword@mycluster0-shard-00-00-wpeiv.mongodb.net:27017,mycluster0-shard-00-01-wpeiv.mongodb.net:27017,mycluster0-shard-00-02-wpeiv.mongodb.net:27017/admin?ssl=true&replicaSet=Mycluster0-shard-0&authSource=admin";
-var url = 'mongodb://cosmos:'+dbpassword+'@cluster0-shard-00-00-oe5ks.mongodb.net:27017,cluster0-shard-00-01-oe5ks.mongodb.net:27017,cluster0-shard-00-02-oe5ks.mongodb.net:27017/?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+var url = 'mongodb://cosmos:'+dbpassword+'@cluster0-shard-00-00-oe5ks.mongodb.net:27017,cluster0-shard-00-01-oe5ks.mongodb.net:27017,cluster0-shard-00-02-oe5ks.mongodb.net:27017/'+dbName+'?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 var postData = require('../data/posts.json');
 var bodyParser = require('body-parser');
-var database = null
+var db = null
 
-MongoClient.connect(url, function(err, db){
+
+MongoClient.connect(url, function(err, database){
   if(err){
     console.log(err);
   }
-  console.log('connected successfully to database');
-  console.log('db:prior'+db)
-  database = db
-  console.log('db:after'+db)
-  findDocument(db, function(){
-    db.close();
-  })
+  console.log('connected successfully to database'); 
+  db = database
 });
 
-function insertPost(db, postData, callback){
-  console.log('here')
-  var collection = db.collection('users');
-  console.log("COL:"+collection)
-  var post = postData.text
-  collection.insert({"name":post})
-  console.log('here2')
-  callback
-}
-
-function findDocument(db, callback){
-  var collection = db.collection('users');
-  collection.find().toArray(function(err, doc){
-    //console.log(doc);
-    callback
-  })
-}
-
-
-
+//load all the existing post within the last 48 hours
 router.get('/api',function(req, res){
   res.json(postData);
+  //......work in progress
 });
 
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended:false}));
+//add post to database
 router.post('/api',function(req,res){
-  insertPost(database, req.body, function(){
-    database.close()
-  })
-  console.log(req.body);
+  var data = req.body
+  var collection = db.collection('posts');
+  var post = data.text;
+  var username = data.poster;
+  collection.insert({'text_content':post, 'username':username, 'date': new Date()})
 });
+
+
+
 module.exports = router;

c1f82d9fed3859f7fa5304327deb7789acddee9d
Andy Li <andylibc@gmail.com>
2017-07-13 11:11:27 -0700

    changes

--- app/routes/api.js
+++ app/routes/api.js
@@ -9,22 +9,22 @@ var dbName = "cosmosdb";
 var dbpassword = config.password;
 var postData = require('../data/posts.json');
 var bodyParser = require('body-parser');
-var db = null
+var db = null;
 var url = 'mongodb://cosmos:'+dbpassword+'@cluster0-shard-00-00-oe5ks.mongodb.net:27017,cluster0-shard-00-01-oe5ks.mongodb.net:27017,cluster0-shard-00-02-oe5ks.mongodb.net:27017/'+dbName+'?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
 //var url = 'mongodb://adam:'+dbpassword+'@ds151702.mlab.com:51702/cosmosdb'
-
+var datapost = null;
 
 MongoClient.connect(url, function(err, database){
   if(err){
     console.log(err);
   }
   console.log('connected successfully to database'); 
-  db = database
+  db = database;
 
   //load all the existing posts within the last 48 hours
   getData(function(){
-    db.close()
-  })
+    db.close();
+  });
 });
 
 
@@ -35,20 +35,36 @@ function getData(callback){
     {
       $gte:(new Date((new Date()).getTime()-(15 * 48 * 60 * 60 * 1000)))
     }
-  }).sort({ "date": -1 }).toArray(function(err, docs){
-    console.log(docs)
+  }).sort({ "date": -1 }).toArray(function(err, data){
+    datapost = data
     callback
   });
 }
 
 
 
+router.get('/api',function(req, res){
+  //  getData(function(){
+  //   db.close();
+  // });
 
+  //console.log(datapost)
 
-// router.get('/api',function(req, res){
-  
+  datapost.forEach(function(item){
+    //console.log(item)
+    var text = item.text_content
+    var username = item.username
+    var date = item.date
+    formatDate(date)
+  });
 
-// });
+  res.json(datapost);
+});
+
+function formatDate(date){
+  var formatedDate = 
+  console.log(date)
+}
 
 
 //add post to database
@@ -71,16 +87,6 @@ module.exports = router;
 
 
 
-
-// function getRecentPosts(db, callback){
-//   var recentPosts = '';
-//   var collection = db.collection('posts');
-//   collection.find().sort({'date':-1}).toArray((err,doc)=>{
-//     console.log(doc); //TODO return doc when function called
-//     callback
-//   });
-//   return recentPosts;
-// }
 
 
 

58355dad65507133a01c98f3c7e5eb332cc3550d
Andy Li <andylibc@gmail.com>
2017-07-15 19:55:53 -0700

    merge

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/learning_mongo';
 var bodyParser = require('body-parser');
 var config = require('../data/config');
 var dbName = "cosmosdb";

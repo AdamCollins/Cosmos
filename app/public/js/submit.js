efa0dc93aa76817f09f272722af9c48dde9098de
AdamCollins <adamcollinssecure@gmail.com>
2017-09-11 22:38:37 -0700

    Disabled notifications on non-firefox/chrome browsers

--- app/public/js/submit.js
+++ app/public/js/submit.js
@@ -2,37 +2,56 @@ $("form.submitPanel").on('submit', function(e) {
 
   e.preventDefault();
   var text = $('#PostTextArea').val().trim();
-    if(!text)
-      return
+  if (!text)
+    return
   $('#PostTextArea').val('');
   $('#CharCount').text('');
-  console.log(1);
-  OneSignal.push(function() {
-    console.log(1.5);
-    OneSignal.getUserId(function(userId) {
-      console.log(1.6);
-      console.log("OneSignal User ID:", userId);
-      var params = {
-        'text_content': text,
-        'OneSignalUserId':userId
-      }
-      console.log(2);
-      $.ajax({
-        method: 'post',
-        url: '/api',
-        data: params,
-        datatype: 'json',
-        success: function(post) {
-          console.log(3);
-          createPost(post, true);
-          $('div.post.hidden').hide().removeClass('hidden').fadeIn(800);
-          openReply();
-        },
-        error: function(err) {
-          alert('oops something went wrong')
+
+  var firefox = navigator.userAgent.indexOf('Firefox') > -1;
+  var chrome = navigator.userAgent.indexOf('Chrome') > -1;
+  if(firefox | chrome){
+    OneSignal.push(function() {
+      OneSignal.getUserId(function(userId) {
+        console.log("OneSignal User ID:", userId);
+        var params = {
+          'text_content': text,
+          'OneSignalUserId': userId
         }
+        console.log(2);
+        $.ajax({
+          method: 'post',
+          url: '/api',
+          data: params,
+          datatype: 'json',
+          success: function(post) {
+            console.log(3);
+            createPost(post, true);
+            $('div.post.hidden').hide().removeClass('hidden').fadeIn(800);
+            openReply();
+          },
+          error: function(err) {
+            alert('oops something went wrong')
+          }
+        });
       });
     });
-  });
+  }else{
+    $.ajax({
+      method: 'post',
+      url: '/api',
+      data: params,
+      datatype: 'json',
+      success: function(post) {
+        console.log(3);
+        createPost(post, true);
+        $('div.post.hidden').hide().removeClass('hidden').fadeIn(800);
+        openReply();
+      },
+      error: function(err) {
+        alert('oops something went wrong')
+      }
+    });
+  }
+
   console.log(4);
 });

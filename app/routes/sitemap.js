c5def5810f8029794062743c0be1552b74a397e0
AdamCollins <adamcollinssecure@gmail.com>
2017-09-15 13:21:16 -0700

    Added sitemap

--- app/routes/sitemap.js
+++ app/routes/sitemap.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var xml = require('xml');
 router.get('/sitemap.xml', function(req, res) {
   res.type('xml/plain');
   res.render(xml('sitemap.ejs'));
